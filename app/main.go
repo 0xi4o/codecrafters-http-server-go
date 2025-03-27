be8b6f885a8f965e15b08ba87a3bcbb5ef59eeac
Ilango <rajagopalilango@gmail.com>
2025-03-27 08:21:07 +0530

    Stage 3: extract url path and respond

--- app/main.go
+++ app/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	tcpAddr := net.TCPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 4221,
+	}
+	l, err := net.ListenTCP("tcp", &tcpAddr)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	tcpConn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer tcpConn.Close()
+	fmt.Printf("Listening on port: %d...\n", tcpAddr.Port)
+	for {
+		tcpConn, err := l.AcceptTCP()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	tcpConn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		go func(c net.TCPConn) {
+			fmt.Println("Incoming request...")
+			statusLine, err := bufio.NewReader(&c).ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading: ", err.Error())
+				c.Close()
+				os.Exit(1)
+			}
+			fmt.Printf("Status: %s\n", statusLine)
+			parts := strings.Split(statusLine, " ")
+			if parts[1] == "/" {
+				c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			} else {
+				c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			}
+			c.Close()
+		}(*tcpConn)
+	}
 }
