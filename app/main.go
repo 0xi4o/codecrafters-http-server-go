6357e2a7e3a7c94328812c036c8cefc70f450543
Ilango <rajagopalilango@gmail.com>
2025-03-27 07:54:19 +0530

    Stage 2: simple response

--- app/main.go
+++ app/main.go
@@ -10,17 +10,18 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	tcpConn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer tcpConn.Close()
+
+	tcpConn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
