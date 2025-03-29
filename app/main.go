ec2076040a73b99b9205098172add87f1d0dd24f
Ilango <rajagopalilango@gmail.com>
2025-03-29 05:56:17 +0530

    Stage 5: read header

--- app/main.go
+++ app/main.go
@@ -5,20 +5,16 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 const CRLF = "\r\n"
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	tcpAddr := net.TCPAddr{
 		IP:   net.IPv4(127, 0, 0, 1),
 		Port: 4221,
 	}
-	l, err := net.ListenTCP("tcp", &tcpAddr)
+	listener, err := net.ListenTCP("tcp", &tcpAddr)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -26,7 +22,7 @@ func main() {
 
 	fmt.Printf("Listening on port: %d...\n", tcpAddr.Port)
 	for {
-		tcpConn, err := l.AcceptTCP()
+		tcpConn, err := listener.AcceptTCP()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
@@ -42,18 +38,8 @@ func main() {
 				os.Exit(1)
 			}
 			request := DeserializeRequest(string(r))
-			pathParts := strings.Split(request.Path, "/")
-			switch {
-			case pathParts[1] == "echo":
-				response := SerializeResponse(request)
-				c.Write([]byte(fmt.Sprintf("%s %s%sContent-Type: %s%sContent-Length: %d%s%s%s", response.Version, response.Status, CRLF, response.Headers.ContentType, CRLF, response.Headers.ContentLength, CRLF, CRLF, response.Body)))
-			case request.Path == "/":
-				response := SerializeResponse(request)
-				c.Write([]byte(fmt.Sprintf("%s %s%s%s", response.Version, response.Status, CRLF, CRLF)))
-			default:
-				response := SerializeResponse(request)
-				c.Write([]byte(fmt.Sprintf("%s %s%s%s", response.Version, response.Status, CRLF, CRLF)))
-			}
+			response := SerializeResponse(request)
+			c.Write([]byte(response))
 			c.Close()
 		}(*tcpConn)
 	}
--- app/response.go
+++ app/response.go
@@ -18,30 +18,48 @@ type Response struct {
 	Body    string
 }
 
-func SerializeResponse(request Request) Response {
+func SerializeResponse(request Request) string {
+	if match, _ := path.Match("/user-agent", request.Path); match {
+		body := request.Headers.UserAgent
+		responseHeaders := ResponseHeaders{
+			ContentType:   "text/plain",
+			ContentLength: len(body),
+		}
+		response := Response{
+			Version: request.Version,
+			Status:  fmt.Sprintf("%d %s", http.StatusOK, "OK"),
+			Headers: responseHeaders,
+			Body:    body,
+		}
+		return fmt.Sprintf("%s %s%sContent-Type: %s%sContent-Length: %d%s%s%s", response.Version, response.Status, CRLF, response.Headers.ContentType, CRLF, response.Headers.ContentLength, CRLF, CRLF, response.Body)
+	}
+
 	if match, _ := path.Match("/echo/*", request.Path); match {
 		body := path.Base(request.Path)
 		responseHeaders := ResponseHeaders{
 			ContentType:   "text/plain",
 			ContentLength: len(body),
 		}
-		return Response{
+		response := Response{
 			Version: request.Version,
 			Status:  fmt.Sprintf("%d %s", http.StatusOK, "OK"),
 			Headers: responseHeaders,
 			Body:    body,
 		}
+		return fmt.Sprintf("%s %s%sContent-Type: %s%sContent-Length: %d%s%s%s", response.Version, response.Status, CRLF, response.Headers.ContentType, CRLF, response.Headers.ContentLength, CRLF, CRLF, response.Body)
 	}
 
 	if path.Base(request.Path) == "/" {
-		return Response{
+		response := Response{
 			Version: request.Version,
 			Status:  fmt.Sprintf("%d %s", http.StatusOK, "OK"),
 		}
+		return fmt.Sprintf("%s %s%s%s", response.Version, response.Status, CRLF, CRLF)
 	}
 
-	return Response{
+	response := Response{
 		Version: request.Version,
 		Status:  fmt.Sprintf("%d %s", http.StatusNotFound, "Not Found"),
 	}
+	return fmt.Sprintf("%s %s%s%s", response.Version, response.Status, CRLF, CRLF)
 }
